e84578020020495c6b0e800e16e09712feffeb85
aarxiss <grecanikolay06@gmail.com>
2024-11-29 20:30:14 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "uk_UA");
+	///////////////////////////////////////////////////////////////////////
+					  //канкулятор 2 чисел//
+	/*int a, b, selection;
+	cout << "Введите 2 числа" << endl;
+	cin >> a >> b;
+
+	cout << "Выберите математическую операцию" << endl<<
+	"1. Сложение" << endl<<
+	"2. Вычитание" << endl <<
+	"3. Умножение" << endl <<
+	"4. Деление" << endl;
+
+	cin >> selection;
+
+	switch (selection)
+	{
+	case 1:
+		cout << " Результат сложения" << a + b << endl;
+		break;
+	case 2:
+		cout << " Результат вычитания" << a - b << endl;
+		break;
+	case 3:
+		cout << " Результат умножения" << a * b << endl;
+		break;
+	case 4:
+		cout << " Результат деления" << (double) a / b << endl;
+		break;
+
+	default:
+		cout << "Ошибка! Пункт меню отсутсвтует!" << endl;
+		break;
+	}*/
+
+	/////////////////////////////////////////////////////////////////////////
+								   //WHILE//
+//	int symbolCount;
+//	char symbol;
+//	int lineType;
+//	int index = 0;
+//
+//	cout << "Введите количество символов в линии" << endl;
+//	cin >> symbolCount;
+//	cout << "Введите символ" << endl;
+//	cin >> symbol;
+//	
+//	cout << "Выберите тип линии: " << endl <<
+//		"1 - вертикальная линия" << endl <<
+//		"2 - горизонатльная линия " << endl;
+//
+//	cin >> lineType;
+//
+//	if (lineType != 1 && lineType != 2)
+//	{
+//		cout << "Не коректный тип линии!" << endl;
+//		return 2; 
+//	}
+//
+//	while (index < symbolCount)
+//	{
+//		if (lineType == 1)
+//		{
+//
+//			cout << symbol << endl;
+//		}
+//
+//
+//		if (lineType == 2)
+//		{
+//			cout << symbol;
+//		}
+//		index++;
+//	}
+//	cout << endl << endl;
+//	
+//}
+	/////////////////////////////////////////////////////////////////////////
+								  //FOR
+	//int i = 0;
+
+	/*cout << "1й цикл" << endl;
+	for (int i = 0, j = 10; i < 10 && j!=5; i++, j--)
+	{
+		cout << "переменная i = " << i << endl;
+		cout << "переменная j = " << j << endl;*/
+		//}
+	//cout << "2й цикл" << endl;
+	//for (int i = 0; ; i++)
+	//{
+	//	cout << "переменная i = " << i << endl;
+	//	if (i == 1000)
+	//	{
+	//		break;
+	//	}
+	//}
+		/*cout << "2й цикл" << endl;
+		for (; i < 20; i++)
+		{
+			cout << "Переменная i = " << i << endl;
+		}*/
+
+		//int i = 0;
+		//while (true)
+		//{
+		//	cout << "переменная I = " << i << endl;
+		//		i++;
+		//	if (i == 6)
+		//	{
+		//		break;
+		//	}
+		//}
+		//cout << "конец " << endl;
+		//}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (i == 2)
+//		{
+//			continue;
+//		}
+//		cout << "переменная i = " << i << endl;
+//	}
+//}
